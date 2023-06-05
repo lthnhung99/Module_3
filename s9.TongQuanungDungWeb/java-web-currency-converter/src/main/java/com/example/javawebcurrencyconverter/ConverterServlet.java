a3b0ae32d076ba58bbc4116b147a2dc0867eaaf3
lthn99 <lenhung14101999@gmail.com>
2023-06-05 20:44:44 +0700

    [Thực hành] Ứng dụng chuyển đổi tiền tệ - Servlet

--- s9.TongQuanungDungWeb/java-web-currency-converter/src/main/java/com/example/javawebcurrencyconverter/ConverterServlet.java
+++ s9.TongQuanungDungWeb/java-web-currency-converter/src/main/java/com/example/javawebcurrencyconverter/ConverterServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = " ConverterServlet",value = "/convert")
+@WebServlet(name = "ConverterServlet",value = "/convert")
 public class ConverterServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
