d9e983cbc58560c19117eaf723b580463ca0141c
lthn99 <lenhung14101999@gmail.com>
2023-06-05 20:20:26 +0700

    [Thực hành] Tạo trang web để đăng nhập và hiển thị lời chào

--- s9.TongQuanungDungWeb/simple_login/src/main/java/com/example/simple_login/LoginServlet.java
+++ s9.TongQuanungDungWeb/simple_login/src/main/java/com/example/simple_login/LoginServlet.java
@@ -9,13 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "LoginServlet",value = "/login")
+@WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/index.jsp");
-        requestDispatcher.forward(req,resp);
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 
     @Override
